9794e2b2861983a4ba5f51f0a0548294125eb81f
abdullinn4 <abdullin.nargiz@yandex.ru>
2026-01-05 16:59:25 +0300

    project initialize

---
+++ src/main/java/org/example/apexams/ApExamsApplication.java
@@ -0,0 +1,13 @@
+package org.example.apexams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApExamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApExamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apexams/ApExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apexams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApExamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
